2f1e13c662bda384cd1dc3a3edbd28728f58aca6
Leonard Menzel <leonderdunna@gmail.com>
2023-03-22 12:18:41 +0100

    new main-text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule} from "@angular/material/toolbar";
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule} from "@angular/material/sidenav";
+import { MatIconModule} from "@angular/material/icon";
+import { MatCardModule} from "@angular/material/card";
+import { MainTextComponent } from './main-text/main-text.component';
+import { MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainTextComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
